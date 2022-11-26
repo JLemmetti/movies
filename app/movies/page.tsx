43cc0078c8103782a768031829917ac3f0f787b0
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2022-11-26 12:40:52 +0200

    List movies

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,3 +1,31 @@
+import { format, parseISO } from 'date-fns';
+import { Movie, movieDb } from '../api/db';
+
+const MovieItem = ({
+  movie: { title, link, watchDate, rating, notes },
+}: {
+  movie: Movie;
+}): JSX.Element => (
+  <li className="px-5 py-10 space-y-4 border-2 border-solid rounded border-slate-300 bg-slate-200">
+    <h3 className="text-xl">
+      <a href={link}>{title}</a>
+    </h3>
+    <h3>({format(parseISO(watchDate), 'd.M.Y')})</h3>
+    <p>{notes}</p>
+    <p>{rating}</p>
+  </li>
+);
+
 export default function Page() {
-  return <p>Movies page</p>;
+  return (
+    <>
+      <h2 className="text-3xl">Movies page</h2>
+
+      <ul className="my-5 space-y-5">
+        {movieDb.reverse().map((movie: Movie, index: number) => (
+          <MovieItem movie={movie} key={index} />
+        ))}
+      </ul>
+    </>
+  );
 }
