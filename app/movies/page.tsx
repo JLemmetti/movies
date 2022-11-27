71215acd651e1a421520d75750719c51ba82b3e8
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2022-11-27 19:11:19 +0200

    Add link to JustWatch

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,4 +1,5 @@
 import { format, parseISO } from 'date-fns';
+import Image from 'next/image';
 import { use } from 'react';
 import { Movie, movieDb } from '../api/db';
 import { wait } from '../utils';
@@ -18,6 +19,21 @@ const MovieItem = ({
   <li className="px-5 py-10 space-y-4 rounded bg-slate-200">
     <h3 className="text-xl hover:underline">
       <a href={link}>{title}</a>
+      <div className="relative inline-block w-6 h-6 ml-3 align-middle rounded-full bg-slate-400 hover:bg-slate-600">
+        <a
+          href={`https://www.justwatch.com/fi/etsi?q=${encodeURI(title)}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            className="ml-[7px] mt-[6px]"
+            src="/imgs/jw-logo.svg"
+            width="13"
+            height="13"
+            alt={`Check where to watch ${title} in JustWatch`}
+          />
+        </a>
+      </div>
     </h3>
     <h3>({format(parseISO(watchDate), 'd.M.Y')})</h3>
     <p>{notes}</p>
