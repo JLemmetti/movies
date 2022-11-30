379bf48a0a23fca997405eb38072cb145b5c56e7
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2022-11-30 21:39:33 +0200

    Move movies from page to layout

--- app/movies/layout.tsx
+++ app/movies/layout.tsx
@@ -1,13 +1,37 @@
+import { use } from 'react';
+import { Movie, movieDb } from '../api/db';
+import { wait } from '../utils';
+import EditMovieItem from './edit-movie-item';
+import { MovieItem } from './movie-item';
+
 type Props = {
   children: JSX.Element;
 };
 
+async function getMovies() {
+  await wait(2000); // Prefetched
+  return movieDb;
+}
+
 export default function RootLayout({ children }: Props): JSX.Element {
+  const movies = use(getMovies());
+
   return (
     <div>
-      <h2 className="text-3xl">Movies page</h2>
+      <h2 className="text-3xl">Movies</h2>
+      <p>(Movies watched: {movies.length})</p>
 
-      <div>{children}</div>
+      <EditMovieItem isNew={true} />
+
+      <div className="grid grid-cols-2 gap-4">
+        <ul className="my-5 space-y-5">
+          {movies.reverse().map((movie: Movie, index: number) => (
+            <MovieItem movie={movie} key={index} />
+          ))}
+        </ul>
+
+        <div>{children}</div>
+      </div>
     </div>
   );
 }
--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,26 +1,7 @@
-import { use } from 'react';
-import { Movie, movieDb } from '../api/db';
-import { wait } from '../utils';
-import EditMovieItem from './edit-movie-item';
-import { MovieItem } from './movie-item';
-
-async function getMovies() {
-  await wait(200);
-  return movieDb;
-}
-
 export default function Page() {
-  const movies = use(getMovies());
-
   return (
-    <>
-      <EditMovieItem isNew={true} />
-
-      <ul className="my-5 space-y-5">
-        {movies.reverse().map((movie: Movie, index: number) => (
-          <MovieItem movie={movie} key={index} />
-        ))}
-      </ul>
-    </>
+    <p className="px-5 py-10 my-5 space-y-4 text-2xl rounded  bg-slate-200">
+      Pick a movie
+    </p>
   );
 }
