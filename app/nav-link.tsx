2d3ab9c9e2ea627a7aa805314535e22e90f3d076
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2023-03-28 19:14:31 +0300

    Protect Movies route from unauthorised users

--- app/nav-link.tsx
+++ app/nav-link.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { useSelectedLayoutSegments } from 'next/navigation';
 
@@ -9,11 +10,15 @@ type Props = {
 };
 
 export default function NavLink({ href, children }: Props): JSX.Element {
+  const { data: session } = useSession();
   // Workaround for https://github.com/vercel/next.js/pull/42299
   const layoutSegments = useSelectedLayoutSegments();
   const segment = layoutSegments[0] ?? '';
 
   const active = href === `/${segment}`;
+
+  // Hide navigation if user is not authorized
+  if (!session) return <div />;
 
   return (
     <Link className={active ? 'underline underline-offset-4' : ''} href={href}>
